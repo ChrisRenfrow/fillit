38e50478d4dd875a29dfe18dba3acd480fe5be80
kdavis <mr_kdavis@sbcglobal.net>
2016-10-06 14:24:34 -0700

    renamed input_loader to main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/06 14:23:48 by kdavis            #+#    #+#             */
+/*   Updated: 2016/10/06 14:23:51 by kdavis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "fillit.h"
+
+/*
+** Pseudo code: 22: if not exactly one argument activate function to print usage
+** 25: Input validation function, tie it in with reading the pieces.
+** 26: Send pieces and number of pieces into the puzzle solver.
+*/
+
+int	main(int argc, char **argv)
+{
+	t_piece	**pieces;
+	char	**puzzle;
+	int		piecenbr;
+
+	if (argc == 2)	
+	{
+		pieces = input_validator(argv[1], &piecenbr);
+		puzzle = fillit(pieces, piecenbr);	
+	}
+	else
+		ft_fillit_usage;
+	return (0);	
+}
