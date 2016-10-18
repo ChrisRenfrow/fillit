ff65932a0ba79313ecd7bafa0cfa9b504b740ddc
kdavis <kyweda@gmail.com>
2016-10-18 10:46:11 -0700

    Added preliminary algorithm. Still needs optimized and cleaned up

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/04 16:11:40 by kdavis            #+#    #+#             */
-/*   Updated: 2016/10/14 11:56:47 by crenfrow         ###   ########.fr       */
+/*   Updated: 2016/10/18 10:45:22 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,38 @@ const char *g_blockdefine[] = {		"#...#...#...#", 	//VT I
 									"#...##..#",		//RT T
 									"###..#",			//DN T
 									".#..##...#" };		//LT T
+typedef unsigned long long	t_ull;
 
-typedef	struct			s_piece
+typedef	struct				s_puzzle
 {
-	unsigned char		label;
-	unsigned int		placed;
-	unsigned long long	map[4];
-}						t_piece;
+	int						sq_size;
+	int						pnbr;
+	int						shift;
+	int						mrow;
+	int						eb_max;
+	int						*po;
+}							t_puzz;
+
+typedef	struct				s_piece
+{
+	unsigned long long		r[4];
+	unsigned long long		a[4];
+	int						placed;
+	int						order;
+	unsigned char			label;
+}							t_piece;
+
+//Kyle functions// Delete this line
+int							check_map(t_piece p, t_ull *map, t_puzz l);
+t_piece						edit_map(t_piece p, t_ull *map, t_puzz l, int status);
+int							count_empty(t_ull row, t_puzz l);
+t_ull						*fit_pieces(t_piece *p, t_ull *map, int eb_nbr, t_puzz l);
+t_ull						*move_bits(t_piece *p, t_ull *map, int eb_nbr, t_puzz l);
+t_ull	 	 	 	 	 	*move_next_line(t_piece *p, t_ull *map, int eb_nbr, t_puzz l);
+t_ull						*shift_pieces(t_piece *p, t_ull *map, int eb_nbr, t_puzz l);
+t_ull						*map_maker(t_piece *pieces, t_puzz legend);
+t_ull						*eb_calculator(t_piece *pieces, t_ull *map, t_puzz l);
+
 
 int 	ct_pieces(char *input);
 int 	is_valid_block(char *input);
---
+++ src/map_check.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_check.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/17 10:26:52 by kdavis            #+#    #+#             */
+/*   Updated: 2016/10/17 12:33:17 by kdavis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "fillit.h"
+
+/*
+** Check_bottom checks to make sure that the piece won't surpass the bottom
+** border.
+*/
+
+static int	check_bottom(t_piece p, t_puzz l)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 4)
+		if ((l.mrow + i >= l.sq_size) && p.r[i] != 0)
+			return (0);
+	return (1);
+}
+
+/*
+** Check_border checks to make sure that the piece won't overflow the
+** far right border.
+*/
+
+static int	check_border(t_piece p, t_puzz l)
+{
+	int	brd;
+	int	i;
+
+	brd = 1 << l.sq_size;
+	i = -1;
+	ft_putstr("Border value:"); //
+	ft_putnbr((int)brd);//
+	ft_putchar('\n');//
+	while (++i < 3)
+		if (p.r[i] / brd != 0)
+			return (0);
+	return (1);
+}
+
+/*
+** Check_zone checks to make sure that the piece doesn't overlap with any
+** pieces that have been placed already.
+*/
+
+static int	check_zone(t_piece p, t_ull *map, t_puzz l)
+{
+	int	i;	
+
+	i = -1;
+	while (++i < 4)
+		if ((p.r[i] & map[l.mrow + i]) != 0)
+			return (0);
+	return (1);
+}
+
+/*
+** Check_map calls on the check functions to determine if the piece can be
+** placed in this particular location on the map.
+*/
+
+int	check_map(t_piece p, t_ull *map, t_puzz l)
+{
+	int	result;
+
+	ft_putendl("10 checking if can paste");//
+	result = check_zone(p, map, l) * check_border(p, l) * check_bottom(p, l); 
+	if (result == 0)
+		ft_putendl("11 could not paste"); //
+	return (result);
+}
